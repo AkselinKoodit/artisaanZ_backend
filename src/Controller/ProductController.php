989ed4033cd418aac2524eb4b0246af377241c2c
Akseli <s2100140@edu.bc.fi>
2021-05-26 12:07:26 +0300

    products/all route working and json data added

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,5 +16,24 @@ class ProductController extends AbstractController
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/ProductController.php',
         ]);
+    }
+    /**
+     * @Route("/product/{id}", name="get-a-product", methods={"GET"})
+     */
+    public function product($id, Request $request) {
+        return $this->json([
+            'message'=>'Requesting recipe with id'. $id,
+            'page'=> $request->query->get('page')
+        ]);
+    }
+
+    /**
+     * @Route("/products/all", methods={"GET"})
+     */
+    public function getAllProducts() {
+        $rootPath = $this->getParameter('kernel.project_dir');
+        $products = file_get_contents($rootPath . '/Resources/products.json');
+        $decodedProducts = json_decode($products, true);
+        return $this->json($decodedProducts);
     }
 }
