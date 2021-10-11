678e320289d3d41072ebc0a112a5e3a2415f8798
Akseli <s2100140@edu.bc.fi>
2021-10-11 13:22:27 +0300

    added paymentController and installed Stripe

---
+++ src/Controller/PaymentController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller;
+
+use http\Url;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class PaymentController extends AbstractController
+{
+
+    #[Route('/payment', name: 'payment')]
+    public function index(): Response
+    {
+        return $this->render('payment/index.html.twig', [
+            'controller_name' => 'PaymentController',
+        ]);
+    }
+    #[Route('/checkout', name: 'checkout')]
+    public function checkout($stripeSK): Response
+    {
+        \Stripe\Stripe::setApiKey($stripeSK);
+
+        $session = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [[
+                'price_data' => [
+                    'currency' => 'eur',
+                    'product_data' => [
+                        'name' => 'kännykkäpussi',
+                    ],
+                    'unit_amount' => 2000,
+                ],
+                'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            'success_url' => $this->generateUrl('success_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'cancel_url' => $this->generateUrl('cancel_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
+        ]);
+
+        //return $response->withHeader('Location', $session->url)->withStatus(303);
+        return $this->redirect($session->url, 303);
+    }
+
+    #[Route('/success-url', name: 'success_url')]
+    public function successUrl(): Response
+    {
+        return $this->render('payment/success.html.twig', []);
+    }
+    #[Route('/cancel-url', name: 'cancel_url')]
+    public function cancelUrl(): Response
+    {
+        return $this->render('payment/cancel.html.twig', []);
+    }
+
+}
